582718a88fad6920993b49c149c18ef6a3c7e301
Alex Chen <achen831008@gmail.com>
2017-10-23 11:02:52 -0400

    implemented heatmap using all sp500 stock correlation

--- getSP500.py
+++ getSP500.py
@@ -5,6 +5,11 @@ import pandas as pd
 import pandas_datareader.data as web
 import pickle
 import requests
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+
+style.use('ggplot')
 
 def save_sp500_tickers():
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -72,4 +77,30 @@ def compile_data():
     print(main_df.head())
     main_df.to_csv('sp500_joined_closes.csv')
 
-compile_data()
+def heatmap():
+    df = pd.read_csv('sp500_joined_closes.csv')
+    df_corr = df.corr()
+
+    data = df_corr.values
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+    fig.colorbar(heatmap)
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap.set_clim(-1, 1)
+    plt.tight_layout()
+    plt.show()
+
+
+heatmap()
