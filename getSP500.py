4bd3ca0ea690f26b929bb0f677d1455cff72bb11
Alex Chen <achen831008@gmail.com>
2017-10-22 22:23:19 -0400

    upload all s&p500 data

--- getSP500.py
+++ getSP500.py
@@ -22,4 +22,54 @@ def save_sp500_tickers():
     print(tickers)
     return tickers
 
-def get_data_from_yahoo()
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open('sp500tickers.pickle', 'rb') as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2000,1,1)
+    end = dt.datetime(2016, 12, 31)
+
+    for ticker in tickers:
+        print(ticker)
+
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)) and ticker not in ['BRK.B', 'BF.B']:
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
+
+
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        if ticker in ['BRK.B', 'BF.B']:
+            continue
+
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df .set_index('Date', inplace=True)
+
+        df.rename(columns={'Adj Close': ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closes.csv')
+
+compile_data()
